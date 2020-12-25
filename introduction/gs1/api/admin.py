1fbe7b3963f7e7c8c2ad835e2beb9a52f3b2920c
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-12-25 14:50:42 +0530

    Update admin.py

--- introduction/gs1/api/admin.py
+++ introduction/gs1/api/admin.py
@@ -3,4 +3,7 @@ from .models import Student
 # Register your models here.
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_disply = ['id', 'name', 'roll', 'city']
+    # localhost:8000/admin/student
+    # student table view with below fields in admin
+    # list_display is a builtin property, used to display required fields of student table in admin view
+    list_display = ['id', 'name', 'roll', 'city']
