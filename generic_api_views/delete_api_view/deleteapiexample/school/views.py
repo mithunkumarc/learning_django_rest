e8db43470c24dfa3594acccc87fff9ca018e7035
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-12-31 12:24:36 +0530

    Add files via upload

---
+++ generic_api_views/delete_api_view/deleteapiexample/school/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+# from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.views import APIView
+from rest_framework.generics import DestroyAPIView
+
+class StudentDelete(DestroyAPIView):
+	queryset = Student.objects.all()
+	serializer_class = StudentSerializer
