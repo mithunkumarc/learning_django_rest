6af7cccf1b56d2e1f0b690f5fcdbda0800cfb403
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2021-01-01 18:25:44 +0530

    Add files via upload

---
+++ filters/ordering_fields/orderingfilterexample/school/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
+from rest_framework import filters
+
+class StudentList(ListAPIView):
+  queryset = Student.objects.all()
+  serializer_class = StudentSerializer
+  filter_backends = [filters.OrderingFilter]
+  # if you don't mention ordering_fields : ascending descending ordering works for all fields
+  ordering_fields = ['name'] # works only for name
+  # ordering_fields = ['name', 'city']
