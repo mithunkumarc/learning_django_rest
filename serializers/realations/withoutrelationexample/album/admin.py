61a9c3998cb983189d127673f68c8ee9a961fc5b
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2021-01-02 10:46:25 +0530

    Add files via upload

---
+++ serializers/realations/withoutrelationexample/album/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Singer, Song
+# Register your models here.
+
+@admin.register(Singer)
+class SingerAdmin(admin.ModelAdmin):
+	# display below fields in admin UI
+	list_display = ['id', 'name', 'gender']
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+	# display below fields in admin UI
+	list_display = ['id', 'title', 'singer', 'duration']
